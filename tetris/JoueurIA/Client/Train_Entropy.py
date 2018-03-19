c035745181228ef522ee01d743213202f32a1ad4
projnao <yannis.bendiouis@gmail.com>
2018-03-19 15:59:50 +0100

    Ajout des commentaires pour tout le fichier

--- tetris/JoueurIA/Client/Train_Entropy.py
+++ tetris/JoueurIA/Client/Train_Entropy.py
@@ -15,7 +15,32 @@ from JoueurIA.Client import Stats
 
 
 class Train_Entropy(ClientInterface.ClientInterface):
+    """ Cette classe permet de lancer un entrainement pour une IA utilisant la méthode d'enthropie croisée.
+    Cet entrainement permettra de configurer une famille de vecteur de poids. Chacun de ces poids étant associé à une heuristique.
+    De génération en génération, les vecteurs de poids converge vers une IA sensée être performante.
+    C'est la meilleuir méthode de notre projet.
+    Elle nécessite donc l'usage d'heuristiques.
+
+    Pour pouvoir être lancée sur le serveur, elle se doit d'hériter de la Classe ClientInterface."""
+
     def __init__(self, name, heuristic = [], load_file = None, selection_size = 10, population_size = 50, evaluate_size = 5, nb_generation = 10, is_stats = False, file_stats = None):
+        """ Paramètres :
+            - name : String
+
+            Optionnels :
+            - heuristic : list de String : Nom des heuristics choises si l'on souhaite créer une nouvelle IA
+            - load_file : String : Si l'on charge une ia, nom du fichier
+            /!\ Si ni heuristic, ni load_file n'est utilisé, aucune IA ne peut se créer. 
+            - population_size : int : Nombre d'individu dans une génération
+            - selection_size : int : Nombre d'individu sélectionnés dans chaque génération pour la prochaine
+            - evaluate_size : int : Nombre de partie jouer par individu
+            - nb_generation : int : Nombre de génération pour un entrainement
+            - is_stats : boolean : activation d'un traqueur de statistique pendant l'entrainement.
+            - file_stats : String : Nom du fichier dans lequel écrire les résultats de statistique si is_stats = True
+
+
+            Créer une nouvelle instance de Train_Entropy."""
+
         super().__init__(name,load_file)
         self.weights = list()
         self.heuristic = {}
@@ -43,6 +68,14 @@ class Train_Entropy(ClientInterface.ClientInterface):
             raise Exception("ERROR : Aucun fichier n'a été passé en paramètre pour charger une Level et aucune heuristique n'a été définie, l'Level ne peut se créer.")
 
     def define_heuristic(self, heuristic_list) :
+        """ Paramètres :
+             - heuristic_list : list de String : Nom des heuristics sélectionnées pour créer une nouvelle heuristique
+
+            Retour : 
+                None
+
+            Ajoute les heuristics de heuristic_list dans self.heuristic. """
+
         for h in heuristic_list : 
             if h == "line_transition" :
                 self.heuristic[h] = H.line_transition
@@ -65,6 +98,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
         
 
     def generate_population(self):
+        """ Génère une population aléatoire """
         #si l'Level est déjà définie
         if self.weights is None:
             self.population = [np.random.randn(len(self.heuristic)) for _ in  range(self.population_size)]
@@ -75,6 +109,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
                 self.population.append(np.random.randn(len(self.heuristic)))
     
     def make_selection(self):
+        """ Procède à la sélection des meilleurs individus dans la population """
         population = []
         score = []
         for _ in range(self.selection_size):
@@ -87,6 +122,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
         self.score = score
 
     def reproduction(self):
+        """ Procède à la reproduction des individus actuels et les remplace par la nouvelle population obtenue"""
         ave = np.average(self.population,axis=0,weights = self.score)
         var = np.cov(self.population,rowvar=False)
         self.population = []
@@ -94,6 +130,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
             self.population.append(np.dot(np.random.randn(len(self.heuristic)),var) + ave)
 
     async def evaluate(self):
+        """ Lance les parties sur le Serveur contre IA level 3 (4-heuristic)"""
         self.score = [0 for i in range(len(self.population))]
         for i in range(len(self.population)):
             self.current_game_is_finish = False
@@ -109,6 +146,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
                 self.current_game_is_finish = False
     
     def mutation(self):
+        """ Procède à des mutations aléatoires parmis les individus, probabilité de mutation actuelle 1/5"""
         for vect in self.population:
             #une chance sur 5 de subir une mutation
             if random.random() < 0.2 :
@@ -121,6 +159,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
                     vect[ind] += random.random()*(-0.2)
 
     async def train(self):
+        """ Lance l'entrainement de notre IA Génétique"""
         if self.is_stats :
             self.my_stats = Stats.Stats()
             self.pid_stats = await self.my_stats.observe()
@@ -145,9 +184,21 @@ class Train_Entropy(ClientInterface.ClientInterface):
         self.save()
 
     async def play(self, state):
+        """ Paramètres :
+            - state : State.State : Etat du tetris
+
+            Retourne le meilleur play pour l'ia self.current_eval"""
         return H.best_move(list(self.heuristic.values()),self.population[self.current_eval],state)
 
     def on_finished_game(self,data):
+        """ Paramètres :
+            - data : dictionnaire : Message reçu de la part du serveur
+
+            A la fin de chaque partie, calcul l'efficacité de du vecteur qui a joué :
+            ajoute à self.score[self.current_eval] la différence de points entre les 
+            deux joueurs (positives : victoires, négatives : défaites)"""
+
+            Retourne 
         ind = self.my_client.ids_in_games[data["gid"]][0]
         diff = data["score"][ind] - data["score"][(ind+1)%2]
         diff_square = np.power(diff, 2)
@@ -158,6 +209,9 @@ class Train_Entropy(ClientInterface.ClientInterface):
         self.current_game_is_finish = True
 
     def save(self):
+        """Sauvegarde le meilleurs vecteur de poids actuel, 
+        Le sauvegarde s'écris dans le fichier self.file s'il est défini, 
+        sinon dans save/entropy_X.save où X = premier entier disponible dans la liste de fichier de save/"""
         print("Would you like to save this current AI ? (Y/n)")
         rep = input()
         file_name = ""
@@ -191,6 +245,7 @@ class Train_Entropy(ClientInterface.ClientInterface):
             print("Current Level saved in : ", file_name)
         
     def load(self):
+        """Charge une IA à partir de self.file"""
         #on vérifie que le fichier existe :
         if os.path.isfile(self.file) :
             f = open(self.file, "r")
@@ -210,7 +265,18 @@ class Train_Entropy(ClientInterface.ClientInterface):
 #------------------------ Pour deux IAs -------------------------------------------
 
 
-async def evaluate_two_ias(ia_one, ia_two, is_stats, pid_stats):
+async def evaluate_two_ias(ia_one, ia_two, is_stats = False, pid_stats = None):
+    """ Paramètres :
+        -ia_one : instance Train_Entropy : première famille d'ia génétique
+        -ia_two : instance Train_Entropy : seconde famille d'ia génétique
+
+        Paramètres Optionnels :
+        -is_stats : boolean : Flag pour indiqué si l'on souhaite être traqué par un observateur de statistique pendant
+        l'entrainement des deux ias.
+        -pid_stats : pid de l'observateur Statistique
+
+        Lance les parties sur le serveurs pour les deux ias."""
+
     ia_one.score = [0 for i in range(len(ia_one.population))]
     ia_two.score = [0 for i in range(len(ia_two.population))]
 
@@ -264,6 +330,19 @@ async def evaluate_two_ias(ia_one, ia_two, is_stats, pid_stats):
 
 
 async def train_two_ias(ia_one, ia_two, is_stats = False, file_stats = None):
+    """ Paramètres :
+        -ia_one : instance Train_Entropy : première famille d'ia génétique
+        -ia_two : instance Train_Entropy : seconde famille d'ia génétique
+
+        Paramètres Optionnels :
+        -is_stats : boolean : Flag pour indiqué si l'on souhaite être traqué par un observateur de statistique pendant
+        l'entrainement des deux ias.
+        -pid_stats : pid de l'observateur Statistique
+
+        Entraine deux familles d'IA l'une contre l'autre.
+        Pour chaque génération, chaque individu de chaque famille va jouer 'self.evaluate_size' parties contre l'autre famille.
+        Ainsi, pour chaque partie, un individu pioche une IA au hasard dans la famille opposé et joue contre"""
+
     if ia_one.nb_generation != ia_two.nb_generation :
         return
 
@@ -330,10 +409,10 @@ if __name__ == "__main__":
     #genetic_ia = Train_Entropy("genetic", load_file = "./backup/6_heuristic.save", is_stats = stats, file_stats = my_file_stats)
     ia_one = Train_Entropy("genetic", heuristic=["line_transition", "column_transition", "holes",  \
         "wells", "score", "height", "hidden_empty_cells", "erosion"],  \
-        selection_size = 2, population_size = 5, evaluate_size = 1, nb_generation = 3)
+        selection_size = 5, population_size = 25, evaluate_size = 4, nb_generation = 10)
     ia_two = Train_Entropy("genetic", heuristic=["line_transition", "column_transition", "holes",  \
         "wells", "score", "height", "hidden_empty_cells", "erosion"],  \
-        selection_size = 2, population_size = 5, evaluate_size = 1, nb_generation = 3)
+        selection_size = 5, population_size = 25, evaluate_size = 4, nb_generation = 10)
 
 
     AI_LOOP = asyncio.get_event_loop()
